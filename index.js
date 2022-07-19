4967d99bc834bddabc85c9bcdbec9096331ab8dc
andrea-agosta-salt <andrea.agosta@appliedtechnology.se>
2022-07-19 10:46:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello fellow developer!");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
